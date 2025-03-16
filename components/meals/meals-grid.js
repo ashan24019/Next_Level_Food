e2604c829a47a535c836a2a16cc2d23f33ad84ca
Ashan <ashanvimodh241@gmail.com>
2025-03-16 23:56:16 +0530

    working with outputing meals data & images

---
+++ components/meals/meals-grid.js
@@ -0,0 +1,8 @@
+import classes from './meals-grid.module.css'
+
+export default function MealsGrid({meals}) {
+    return <ul className={classes.meal}>
+        {meals.map(meal => <li key={meal.id}></li>)}
+    </ul>
+
+}
