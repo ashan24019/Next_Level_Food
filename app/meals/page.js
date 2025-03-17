667ea9548e6213020a90b5a665acbcd1b5520ae3
Ashan <ashanvimodh241@gmail.com>
2025-03-17 08:04:59 +0530

    Outputting Meals data & images

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
 
 export default function MealsPage() {
   return (
@@ -15,7 +16,8 @@ export default function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        
+        <MealsGrid meals={[]}/>
+
       </main>
     </>
   );
--- components/meals/meals-grid.js
+++ components/meals/meals-grid.js
@@ -1,8 +1,14 @@
-import classes from './meals-grid.module.css'
+import classes from "./meals-grid.module.css";
+import MealItem from "./meal-item";
 
-export default function MealsGrid({meals}) {
-    return <ul className={classes.meal}>
-        {meals.map(meal => <li key={meal.id}></li>)}
+export default function MealsGrid({ meals }) {
+  return (
+    <ul className={classes.meals}>
+      {meals.map((meal) => (
+        <li key={meal.id}>
+            <MealItem {...meal}/>
+        </li>
+      ))}
     </ul>
-
+  );
 }
