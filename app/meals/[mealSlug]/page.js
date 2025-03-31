b8be03c50fa45109fd240dfddf6aaea13ae262ec
Ashan <ashanvimodh241@gmail.com>
2025-03-31 13:39:52 +0530

    Project Complete

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -3,8 +3,22 @@ import classes from "./page.module.css";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 
+export async function generateMetadata({params}) {
+  return {};
+}
+
+
 export default function MealDetailsPage({ params }) {
   const meal = getMeal(params.mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  return {
+    title: meal.title,
+    description: meal.summary,
+  };
 
   if (!meal) {
     notFound();
